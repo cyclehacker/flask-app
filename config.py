202c49d15e7cf8524116500451c6de1549b57664
cyclehacker <fjeb@mailhost.work>
2020-02-12 12:28:35 +0000

    added migrations

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
     SECRET_KEY = '\x9d\xb9Z|=\x94\xd1^d%\x96\xa6\x1b\xe0\xd4\xf7%\xc0\xcag\xcch\x82\xe8'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print(SQLALCHEMY_DATABASE_URI)
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
